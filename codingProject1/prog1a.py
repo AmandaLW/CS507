45e2919e7ab8287db2a8d2e350463010e07c31d0
AmandaLW <mrs.amandalynnward@gmail.com>
2020-11-05 21:14:23 -0800

    started to tally totals

--- codingProject1/prog1a.py
+++ codingProject1/prog1a.py
@@ -4,7 +4,6 @@ import argparse
 import os,sys
 
 def main():
-   print("I am in the assignment")
 
    formatter = argparse.ArgumentDefaultsHelpFormatter
    parser = argparse.ArgumentParser(description='CS 507 csv reader',
@@ -18,19 +17,53 @@ def main():
    fullFileName=os.path.abspath(fileName)
    processFile(fullFileName)   
 
-   #print("for file: ", fileName)
+
 
 def processFile(fileName):
+
+   totalFP = 0
+   totalTP = 0
+   totalFN = 0
+   totalFunc = 0
+   precision = 0
+   recall = 0
+   f1 =0
 
    with open(fileName,'r') as fn:
       headers=fn.readline().strip().split(',')
       print('Headers = {!s}'.format(headers))
       lineCnt=0
+      #hold =[]
       for line in fn:
          lineCnt+=1
          line = line.strip()  # to remove extra white space
          words = line.split(',')
-         print('{:4d}: {!s}'.format(lineCnt,words))
+         #print('{:4d}: {!s}'.format(lineCnt,words))
+
+         #find and tally up the FP
+         FPnumber = int(words[1])
+         totalFP = totalFP + FPnumber
+         
+         #find and tally up TP
+         TPnumber = int(words[2])
+         totalTP = totalTP + TPnumber
+
+         #find and tally up total FN
+         FNnumber = int(words[3])
+         totalFN = totalFN + FNnumber
+
+   print("For file: ", fileName)
+   print("Total FP: ", totalFP)
+   print("Total TP: ", totalTP)
+   print("Total FN: ", totalFN)
+   print("Total func: ", totalFunc, "\n")
+
+   print("Precision: ", precision)
+   print("Recall: ", recall)
+   print("F1: ", f1)
+         
+         
+         
 
 
          
