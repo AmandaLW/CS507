903a0d05168b8528098905d8cb0fc428d505f9f7
AmandaLW <mrs.amandalynnward@gmail.com>
2020-11-06 23:18:47 -0800

    added ability to read two files

--- codingProject1/prog1c.py
+++ codingProject1/prog1c.py
@@ -3,13 +3,7 @@
 import argparse
 import os,sys
 
-totalFP = 0
-totalTP = 0
-totalFN  = 0
-totalFunc = 0
-precision = 0
-recall = 0
-f1  = 0
+
 
 def main():
 
@@ -20,26 +14,14 @@ def main():
    required = parser.add_argument_group(title='required')
    required.add_argument('file1',  type=str, help='data file to read')
    required.add_argument('file2',  type=str, help='data file to read')
-   
    args = parser.parse_args()
    fileName=args.file1
    fileName2=args.file2
-
    fullFileName=os.path.abspath(fileName)
    fullFileName2=os.path.abspath(fileName2)
-
-   precision1, recall1, f11 = processFile(fullFileName)
-   precision2, recall2, f12 = processFile(fullFileName2)
-
-   precisionAv = (precision1 + precision2)/2
-   recallAv = (precision1 + precision2) /2
-   f1Av = (f11 + f12)/2
-
-   printAv(precisionAv, recallAv, f1Av)
+   avgPrecision, avgRecall, avgF1, totalLines = processFile(fullFileName)
+   printAv(avgPrecision, avgRecall, avgF1, totalLines)
    
-   
-
-
 
 def processFile(fileName):
    totalFP = 0
@@ -49,67 +31,84 @@ def processFile(fileName):
    precision = 0
    recall = 0
    f1  = 0
+   avgPrecision = 0
+   avgRecall = 0
+   avgF1 = 0
+   totalLines = 0
 
    with open(fileName,'r') as fn:
-      headers=fn.readline().strip().split(',')
-      #Sprint('Headers = {!s}'.format(headers))
+      #headers=fn.readline().strip().split(',')
+      headers=fn.readline().strip()
+      
+      
+      addPrec = headers + ",Prec"
+      addRecall = addPrec + ",Recall"
+      builtUpHeader = addRecall + ",F1"
+      
+      #print('Headers = {!s}'.format(headers))
+
+      write_file = "output.csv"
+      with open(write_file, "w") as output:
+        for line in builtUpHeader:
+            output.write(" ".join(line))
+
+      with open(write_file, "a") as output:
+            output.writelines("\n")
+    
+
       lineCnt=0
-      #hold =[]
       for line in fn:
          lineCnt+=1
          line = line.strip()  # to remove extra white space
          words = line.split(',')
-         #print('{:4d}: {!s}'.format(lineCnt,words))
+         #print('{:4d}: {!s}'.format(lineCnt,words))\
+
+         name =words[0]
 
          #find and tally up the FP
          FPnumber = int(words[1])
-         totalFP = totalFP + FPnumber
          
          #find and tally up TP
          TPnumber = int(words[2])
-         totalTP = totalTP + TPnumber
-
+         
          #find and tally up total FN
          FNnumber = int(words[3])
-         totalFN = totalFN + FNnumber
-
+         
          #find and tally up all func
          FuncNumber = int(words[4])
-         totalFunc = totalFunc + FuncNumber
-
-   print("For file: ", fileName)
-   print("Total FP: ", totalFP)
-   print("Total TP: ", totalTP)
-   print("Total FN: ", totalFN)
-   print("Total func: ", totalFunc, "\n")
-
-   precision = totalTP / (totalTP + totalFP)
-   recall = totalTP/(totalTP + totalFN)
-   f1 = (precision * recall) / ((1/2) * (precision + recall))
-
-   precisionRounded = round(precision,2)
-   recallRounded = round(recall,2)
-   f1Rounded = round(f1,2)
-
-   print("Precision: ", precisionRounded)
-   print("Recall: ", recallRounded)
-   print("F1: ", f1Rounded)
-
-   return precisionRounded, recallRounded, f1Rounded
-
-def printAv(precision, recall, f1):
-    print("Avg Precision: ", precision)
-    print("Avg Recall: ", recall)
-    print("Avg F1: ", f1)
-
-
          
-         
+         precision = TPnumber / (TPnumber + FPnumber)
+         recall = TPnumber/(TPnumber + FNnumber)
+         f1 = (precision * recall) / ((1/2) * (precision + recall))
 
+         precisionRounded = round(precision,4)
+         recallRounded = round(recall,4)
+         f1Rounded = round(f1,4)
 
+         avgPrecision = avgPrecision + precisionRounded
+         avgRecall = avgRecall + recallRounded
+         avgF1 = avgF1 + f1Rounded
+         totalLines = lineCnt -1
 
+         thicciBoiLine = words[0] +","+words[1]+","+words[2]+","+words[3]+","+words[4]+","+str(precisionRounded)+","+str(recallRounded)+","+str(f1Rounded)
+       
+         with open(write_file, "a") as output:
+            for line in thicciBoiLine:
+                output.write(" ".join(line))
+         with open(write_file, "a") as output:
+                output.writelines("\n")        
 
-         
+   return avgPrecision, avgRecall, avgF1, totalLines
+
+def printAv(avgPrecision, avgRecall, avgF1, totalLines):
+    precision = avgPrecision/totalLines
+    recall = avgRecall/totalLines
+    f1 = avgF1/totalLines
+    print("Avg Precision: ", round(precision,2))
+    print("Avg Recall: ", round(recall,2))
+    print("Avg F1: ", round( f1,2))
+      
     
 if __name__ == "__main__":
+    
     main()
