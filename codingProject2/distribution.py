352290504ec3dd47355a3ce9cb2a29821d0861f4
AmandaLW <mrs.amandalynnward@gmail.com>
2020-11-22 00:07:45 -0800

    added more plots

--- codingProject2/distribution.py
+++ codingProject2/distribution.py
@@ -16,32 +16,49 @@ def main():
     data3 = []
     data4 = []
 
+    random.seed(4567)
     for i in range(1000):
      data.append(random.uniform(0,100))
 
+    random.seed(34567)
     for i in range(1000):
      data2.append(random.normalvariate(50,10))
 
+    random.seed(98765)
     for i in range(1000):
      data3.append(random.expovariate(1/50))
 
+    random.seed(65432)
     for i in range(1000):
      data4.append(random.betavariate(0.5,0.5))
 
 
 
-    maxY=max(data4)
+    maxY=max(data)
     plt.figure(figsize=(8,8))
     plt.subplot(2,1,1)
-    plt.plot(data4,'ro')
+    plt.plot(data,'ro')
     plt.axis([0,250,0.0,maxY])
-    plt.title('Random Numbers')
+    plt.title('Random Distribution')
 
     numBins=100
     plt.subplot(2,1,2)
-    plt.hist(data4,numBins)
-    plt.title('Histogram of Random Numbers')
-    plt.show()
+    plt.hist(data,numBins)
+    plt.title('Histogram of Random Distribution')
+    plt.savefig('random.png')
+
+    maxY=max(data2)
+    plt.figure(figsize=(8,8))
+    plt.subplot(2,1,1)
+    plt.plot(data2,'ro')
+    plt.axis([0,250,0.0,maxY])
+    plt.title('Uniform Distrobution')
+
+    numBins=100
+    plt.subplot(2,1,2)
+    plt.hist(data,numBins)
+    plt.title('Histogram of Uniform Distribution')
+    plt.savefig('normal.png')
          
 
      
